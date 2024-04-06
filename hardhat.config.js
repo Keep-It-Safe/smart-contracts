579476e8af778945d4fb7f729289bf8607904d59
Dhrupad <thedhrupad@gmail.com>
2024-04-07 03:36:03 +0530

    Redeployed contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,14 @@ module.exports = {
     apothem: {
       url: "https://erpc.apothem.network",
       accounts: [process.env.PRIVATE_KEY],
+    },
+    polygon: {
+      url: `https://polygon-amoy.g.alchemy.com/v2/${process.env.API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY_AMOY]
+    },
+    coinex: {
+      url: "https://testnet-rpc.coinex.net",
+      accounts: [process.env.PRIVATE_KEY_COINEX]
     }
   },
 };
